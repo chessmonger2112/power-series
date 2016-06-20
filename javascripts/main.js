c4635695d3ced43cba5d0fd4046fd14a11311c7b
kyle smith <chessmonger2112@yahoo.com>
2016-06-20 14:54:14 -0700

    refactored where actual function doesn't get called every time an approximation is graphed

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,6 @@ $(function() {
   console.log("suppppp");
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
-
   var tX = 100;
   var tY = 100;
   var magX = 100;
@@ -16,7 +15,6 @@ $(function() {
     context.moveTo(x1 * magX + tX, y1 + tY);
     context.lineTo(x2 * magX + tX, y2 + tY);
     context.strokeStyle = color || "black";
-    // context.strokeStyle = "green";
     context.stroke();
     context.closePath();
   }
@@ -27,7 +25,6 @@ $(function() {
   var deltaX = .1;
   var minX = -10;
   var maxX = 10;
-  var numOfTerms = 1;
 
   var graphFx = function(fX, color)
   {
@@ -37,28 +34,25 @@ $(function() {
       let x2 = x + deltaX;
       let y2 = fX(x + deltaX);
       drawLine({x, y}, {x: x2, y: y2}, color);
-
-      let yA = approximateEX(numOfTerms, x);
-      let yA2 = approximateEX(numOfTerms, x2);
-      drawLine({x, y: yA}, {x: x2, y: yA2});
     }
   }
 
   function makeGraphs(n) {
+    graphFx(eX, "red");
     for (let i = 0; i < n; i ++)
     {
-      graphFx(eX, "red");
-      numOfTerms ++;
+      graphFx(approximateEX.bind(null, i),"black");
     }
   }
 
+  //this functions appoximate e^x
   function approximateEX(n, x) //where n is the amount of desired terms
   {
     var y = 0;
     for (var i = 0; i < n; i ++)
     {
       let num = Math.pow(x, i);
-      let den =   factorial(i);
+      let den = factorial(i);
       let quot = num / den;
       y += quot;
     }
@@ -70,7 +64,6 @@ $(function() {
     console.log(numOfTerms);
     if (numOfTerms)
     {
-      console.log("Sdfsdf");
       makeGraphs(numOfTerms);
     }
   });
